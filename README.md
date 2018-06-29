5c8faec9f776172656b20cb230c71d9142a44189
kleyu <jackob.naskreski@gmail.com>
2018-06-29 23:32:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# REACT CHAT APP
+
+Quick start:
+
+```
+$ npm i
+$ npm run build
+```
+
+Deployed website link:
---
+++ config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b0eb8842-dbfc-4ebc-a9e4-a0bdba78d06c/token";
+const instanceLocator = "v1:us1:b0eb8842-dbfc-4ebc-a9e4-a0bdba78d06c";
+
+export { tokenUrl, instanceLocator }
